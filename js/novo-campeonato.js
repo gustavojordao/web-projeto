6cbb89540f1182d8646443b1afa4e9507c466dde
Gustavo Jordão Nunes <gustavo.jordao@hotmail.com.br>
2016-04-08 20:08:49 -0300

    Adicionando tela de campeonatos do usuário

--- js/novo-campeonato.js
+++ js/novo-campeonato.js
@@ -10,11 +10,9 @@ function init(){
         var divpai = e.target.parentNode.parentNode;
         if(divpai.id == 'add-times'){
             document.getElementById('ja-adicionados').appendChild(e.target.parentNode);
-            //e.target.parentNode.parentNode.removeChild(e.target.parentNode);
         }
         else if(divpai.id == 'ja-adicionados'){
           document.getElementById('add-times').appendChild(e.target.parentNode);
-          //e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 
         }
       }
@@ -41,6 +39,24 @@ function init(){
       var valor = numero.value;
       if(valor > 2)
         numero.value = valor / 2;
+    }
+  );
+
+  //Botão Criar
+  var botaocriar = document.getElementById('botaocriar');
+
+  botaocriar.addEventListener('click',
+    function(e){
+      var adicionados = document.getElementById('ja-adicionados').getElementsByTagName('input');
+      if(adicionados.length == numero.value){
+        window.open('../html/campeonatos.html', '_self');
+      }
+      else if(adicionados.length < numero.value){
+        alert('Selecione mais times.');
+      }
+      else{
+        alert('Selecione menos times.');
+      }
     }
   );
 
