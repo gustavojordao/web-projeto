91d00155babe374b7e89d4584b8693de1180f87d
Gustavo Jordão Nunes <gustavo.jordao@hotmail.com.br>
2016-04-11 23:24:15 -0300

    Finalizado front-end

--- js/comum.js
+++ js/comum.js
@@ -11,7 +11,6 @@ function init(){
   var links = [
     { id: "inicial", link: "index" },
     { id: "campeonatos", link: "campeonatos" },
-    { id: "times", link: "times" },
     { id: "perfil", link: "perfil" },
     { id: "criar-campeonato", link: "novo-campeonato" },
     { id: "cadastrar", link: "cadastro" }
@@ -59,6 +58,39 @@ function init(){
       }
     );
   }
+
+  //Login
+
+  var botaologin = document.getElementById('botao-login');
+  var botaologout = document.getElementById('botao-logout');
+
+  var usuario = document.getElementById('usuario');
+
+  var password = document.getElementById('password');
+
+  var logindiv = document.getElementById('antes');
+  var logadodiv = document.getElementById('depois');
+
+  botaologin.addEventListener('click',
+    function(e){
+      if(usuario.value && password.value){
+        logindiv.classList.add('oculto');
+        logadodiv.classList.remove('oculto');
+        document.getElementById('nomeusuario').innerHTML = usuario.value;
+      }
+    }
+  );
+
+  botaologout.addEventListener('click',
+    function(e){
+      if(usuario.value && password.value){
+        logadodiv.classList.add('oculto');
+        logindiv.classList.remove('oculto');
+        document.getElementById('usuario').value = '';
+        document.getElementById('password').value = '';
+      }
+    }
+  );
 
 }
 
