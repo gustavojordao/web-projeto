dbe42fa3846490c407ea36d7bcd9ea28177c44e4
Gustavo Jordão <gustavo.jordao@hotmail.com.br>
2016-06-21 11:25:24 -0300

    Refatorada pagina de erro

--- server/app.js
+++ server/app.js
@@ -477,3 +477,18 @@ app.get('/pong',
     //request.session.campeonato = undefined;
   }
 );
+
+app.get('*', function(req, res, next) {
+  var err = new Error();
+  err.status = 404;
+  next(err);
+});
+
+// handling 404 errors
+app.use(function(err, req, res, next) {
+  if(err.status !== 404) {
+    return next();
+  }
+
+    res.render('erro.hbs', {});
+});
